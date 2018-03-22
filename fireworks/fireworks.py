c019145151c2e49542577764b72a14a3eda0ad23
Mike Orr <mike@predicate.us>
2018-03-22 17:41:24 -0400

    add readme

--- fireworks/fireworks.py
+++ fireworks/fireworks.py
@@ -1,6 +1,8 @@
 WIDTH = 30
 HEIGHT = 20
 TITLE = 'Fireworks!'
+
+# make the background black
 BACKGROUND = (0,0,0)
 
 def explode(s):
@@ -10,12 +12,11 @@ def explode(s):
 	img.pulse(time=4, size=10).destruct(rand(0.1,4))
 
 def launch():
-	sound('fireworks')
-	p = rand_pos(x_padding=5, y_padding=10)
-	s = shape(CIRCLE, YELLOW, (p[0], WIDTH-5), size=0.1)
-	s.speed(15).move_to(p, callback=lambda: explode(s))
-	callback(launch, rand(0, 3))
+        sound('fireworks')
+        p = rand_pos(x_padding=5, y_padding=10)
+        s = shape(CIRCLE, YELLOW, (p[0], WIDTH-5), size=0.1)
+        s.speed(15).move_to(p, callback=partial(explode, s))
+        callback(launch, rand(0, 3))
 
-callback(launch, 1)	
-
+callback(launch, 1)
 
