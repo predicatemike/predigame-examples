8b5198b8fa58385c65fbf12cfc9394da31e5deac
Mike Orr <mike@predicate.us>
2018-03-22 19:50:50 -0400

    updates

--- pictureframe/frame.py
+++ pictureframe/frame.py
@@ -2,10 +2,15 @@ WIDTH=30
 HEIGHT=20
 FULLSCREEN=True
 
-def change():
-    background()
-
-change()
-callback(change, 10, repeat=FOREVER)
+from time import localtime, strftime
+clock = None
+def update():
+   global clock
+   if clock:
+      clock.destroy()
+   background()
+   clock = text(strftime("%H:%M", localtime()), color=WHITE, pos=(25, 18), size=2)
+update()
+callback(update, 10, repeat=FOREVER)
 
 
